1b49cb4a665c9c958c6078e99896c733349debbb
Jana <12718297+B0Ja@users.noreply.github.com>
2021-01-19 17:32:19 +0100

    Update Reimann V2.py

--- Reimann V2.py
+++ Reimann V2.py
@@ -8,6 +8,20 @@ Plotting the Reimann Zeta function map in imaginary numberspace.
 
 import mpmath
 import matplotlib
+import matplotlib.pyplot as plt
+import timeit
+
+
+points = int(input("Enter number of points: "))
+
+plt.figure(figsize=(10,7))
+start = timeit.default_timer()
+
 
 #This will take a long time to run. For a short run, reduce points to 1000.
-mpmath.cplot(mpmath.zeta, [-10, 10], [0, 100], points = 5000, verbose=False)
+mpmath.cplot(mpmath.zeta, [-10, 10], [0, 100], points = points, verbose=False)
+
+
+stop = timeit.default_timer()
+print('Total run time: ', stop - start, "seconds")
+
